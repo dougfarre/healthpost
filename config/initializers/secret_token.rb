cf3165f805b6afb54a31474c5a4004025b2ee05a
dougfarre <dougfarre@gmail.com>
2013-03-27 00:30:29 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthpost::Application.config.secret_token = 'd68dbb0f22d3013dd7660f10c2f058a872b56e127e46f0202695e584061f475565f0bb4383d0467ffcc8bc5295dded3e6fde08002752e46419cb8b2cd845836c'
